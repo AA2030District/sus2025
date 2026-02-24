73f9abe604fa5c5f23d884c9f6fa858a278f61bc
Dylan Siewert <dsiewert@umich.edu>
2026-02-24 09:08:37 -0500

    deleted old graphs

--- 1_Portfolio_Data.py
+++ 1_Portfolio_Data.py
@@ -151,7 +151,7 @@ fig_pie = px.pie(
 
 # Improve pie chart layout
 fig_pie.update_traces(
-    textposition='inside',
+    textposition='outside',
     textinfo='percent+label',
     hoverinfo='label+percent+value',
     hovertemplate='<b>%{label}</b><br>Square Footage: %{value:,.0f}<br>Percentage: %{percent}<extra></extra>'
@@ -290,7 +290,7 @@ fig = go.Figure(go.Treemap(
 # Update layout
 fig.update_layout(
     title={
-        'text': "Building Energy Performance by Type (2024)",
+        'text': "Building Type EUI Compared to National Median",
         'x': 0.5,
         'xanchor': 'center',
         'font': {'size': 20}
@@ -335,136 +335,7 @@ st.plotly_chart(fig, use_container_width=True)
 
 
 
-# Show only top 30 building types in the chart
-top_30 = df.head(30)
 
-
-site_eui_benchmark = {
-    'Single Family Home': None,
-    'Other - Mall': 101.6,
-    'Vehicle Dealership': 71.9,
-    'Prison/Incarceration': 69.9,
-    'Senior Living Community': 99.0,
-    'Manufacturing/Industrial Plant': None,
-    'Adult Education': 52.4,
-    'Other - Lodging/Residential': 63.6,
-    'Bar/Nightclub': 130.7,
-    'Non-Refrigerated Warehouse': 22.7,
-    'Other - Technology/Science': 40.1,
-    'Fire Station': 63.5,
-    'Other - Services': 47.9,
-    'Mixed Use Property': 40.1,
-    'Ice/Curling Rink': 50.8,
-    'Other - Public Services': 40.1,
-    'Library': 71.6,
-    'Courthouse': 101.2,
-    'Residence Hall/Dormitory': 57.9,
-    'Other - Entertainment/Public Assembly': 56.2,
-    'Parking': None,
-    'Multifamily Housing': 59.6,
-    'K-12 School': 48.5,
-    'Hotel': 63.0,
-    'Other - Utility': 40.1,
-    'Laboratory': 115.3,
-    'Other - Education': 52.4,
-    'Social/Meeting Hall': 56.1,
-    'Wastewater Treatment Plant': 2.89,
-    'Swimming Pool': 50.8,
-    'Food Service': 270.3,
-    'Drinking Water Treatment & Distribution': 2.27,
-    'Retail Store': 51.4,
-    'Museum': 56.2,
-    'Medical Office': 97.7,
-    'Office': 52.9,
-    'Other - Recreation': 50.8,
-    'Police Station': 63.5,
-    'Financial Office': 52.9,
-    'Other - Restaurant/Bar': 325.6,
-    'Residential Care Facility': 99.0,
-    'College/University': 84.3,
-    'Worship Facility': 30.5,
-    'Bowling Alley': 56.2,
-    'Distribution Center': 22.7,
-    'Supermarket/Grocery Store': 196.0,
-    'Other': 40.1,
-    'Strip Mall': 103.5,
-    'Self-Storage Facility': 20.2,
-    'Wholesale Club/Supercenter': 51.4,
-    'Fitness Center/Health Club/Gym': 50.8,
-    'Vehicle Repair Services': 47.9,
-    'Energy/Power Station': 40.1,
-    'Convenience Store without Gas Station': 350.9,
-    'Personal Services (Health/Beauty, Dry Cleaning, etc)': 47.9,
-    'Transportation Terminal/Station': 56.2,
-    'Restaurant': 325.6,
-}
-
-# Bar Chart - Top 30 only
-fig_bar = px.bar(
-    df,
-    x='total_sqft',
-    y='usetype',
-    orientation='h',
-    color_discrete_sequence=['#1f77b4']
-)
-
-fig_bar.update_layout(
-    height=800,
-    xaxis_title="Total Square Feet",
-    yaxis_title="Building Type",
-    yaxis={'categoryorder': 'total ascending'},
-    showlegend=False,
-    title = {
-        'text': "District Property by Square Footage",
-        'font': {'size': 20}
-    }
-)
-
-
-st.plotly_chart(fig_bar, use_container_width=True)
-
-
-# Pie Chart - Top 10 with more margin for labels
-top_10 = df.head(10)
-
-if len(df) > 10:
-    other_sqft = df.iloc[10:]['total_sqft'].sum()
-    other_count = df.iloc[10:]['building_count'].sum()
-    
-    top_10 = pd.concat([
-        top_10,
-        pd.DataFrame([{
-            'usetype': f'Other ({len(df)-10} types)',
-            'total_sqft': other_sqft,
-            'building_count': other_count
-        }])
-    ])
-
-fig_pie = px.pie(
-    top_10,
-    values='total_sqft',
-    names='usetype',
-    hole=0.3
-)
-
-fig_pie.update_layout(
-    height=700,  
-    margin=dict(t=50, b=150, l=50, r=50),  
-    showlegend=False,
-    title={
-        'text': "Largest Property Types by Square Footage",
-        'font': {'size': 20}
-    }
-)
-
-# Make labels smaller so they fit better
-fig_pie.update_traces(
-    textposition='outside',
-    textinfo='percent+label',
-    textfont_size=12 
-)
-
-st.plotly_chart(fig_pie, use_container_width=True)
 
 # Manually inserted data, not taken from SQL/Energy Star
 buildings_data = {
