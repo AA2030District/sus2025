0dee85619259d06c815747d707acf83da581916f
Dylan Siewert <dsiewert@umich.edu>
2026-02-25 14:54:05 -0500

    err handling

--- 2_Building_Data.py
+++ 2_Building_Data.py
@@ -101,7 +101,7 @@ selected_building = st.selectbox(
 )
 
 # Get building info
-selected_espmid = buildings_df.loc[buildings_df['buildingname'] == selected_building, 'espmid'].iloc[0]
+selected_espmid = str(buildings_df.loc[buildings_df['buildingname'] == selected_building, 'espmid'].iloc[0])
 st.write(selected_espmid)
 this_building_query = """
     SELECT 
