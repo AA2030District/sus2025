f3bd1d73cb227fa7f2f71f5350712e7474804f7e
ben rose <bros@2030districts.org>
2026-03-13 12:50:08 -0500

    working on portfolio check

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ home = st.Page("Account_Details.py", title="Account Details")
 page1 = st.Page("1_Portfolio_Data.py", title="Portfolio Data")
 page2 = st.Page("2_Building_Data.py", title="Building Data")
 page3 = st.Page("errorfinder.py", title="Error Finder")
+page4 = st.Page("portfolio check.py", title="Portfolio Analysis")
 
 pg = st.navigation([home, page1,page2,page3])
 
