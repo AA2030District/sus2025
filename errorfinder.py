334ea23a1b1bab83f83f655c68ee16c2295134c7
ben rose <bros@2030districts.org>
2026-03-20 11:55:23 -0500

    work

--- errorfinder.py
+++ errorfinder.py
@@ -208,6 +208,11 @@ with errors:
         "Gaps": [datelist,
         "line 3 \n line 4",
         "line 5 \n line 6",
+        ],
+        "Link": [
+            '<a href="https://google.com" target="_blank">Google</a>',
+            '<a href="https://google.com" target="_blank">Google</a>',
+            '<a href="https://google.com" target="_blank">Google</a>',
         ]}
 
         df = pd.DataFrame(data)
