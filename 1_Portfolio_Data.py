422cbde6e9f18cfe2f37c01db8832af8437cb1e6
ben rose <bros@2030districts.org>
2026-04-03 14:35:19 -0500

    VERSION FIXING

--- 1_Portfolio_Data.py
+++ 1_Portfolio_Data.py
@@ -19,8 +19,18 @@ def apply_white_background(fig):
             bordercolor='black'
         )
     )
-    fig.update_xaxes(color='black')
-    fig.update_yaxes(color='black')
+    fig.update_xaxes(
+        color='black',
+        tickfont=dict(color='black'),
+        title_font=dict(color='black'),
+        linecolor='black'
+    )
+    fig.update_yaxes(
+        color='black',
+        tickfont=dict(color='black'),
+        title_font=dict(color='black'),
+        linecolor='black'
+    )
     fig.update_traces(textfont_color='black')
     for ann in (fig.layout.annotations or []):
         ann.font = dict(color='black')
