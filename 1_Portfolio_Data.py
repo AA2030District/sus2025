d44f0088485ff187aab5650441a71b7478ef9170
ben rose <bros@2030districts.org>
2026-05-03 19:18:47 -0500

    legend

--- 1_Portfolio_Data.py
+++ 1_Portfolio_Data.py
@@ -204,7 +204,7 @@ st.plotly_chart(fig, width="content")
 
 
 
-buildings_df['total_sqft'] = buildings_df['total_sqft'] + 414000
+buildings_df['total_sqft'] = buildings_df['total_sqft'] + 661000
 fig = px.bar(
     buildings_df,
     x='year',
