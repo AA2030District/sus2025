c92f86057a6186bd106420ff663f1a4e58e391d6
ben rose <bros@2030districts.org>
2026-06-02 15:05:04 -0500

    adding fpdf

--- 2_Building_Data.py
+++ 2_Building_Data.py
@@ -504,28 +504,9 @@ if not pie_df.empty:
 else:
     st.warning("No energy data available for 2025 to display pie chart")
 
-np.random.seed(1)
-
-N = 100
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = np.random.rand(N)
-sz = np.random.rand(N) * 30
-
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter(
-        x=x,
-        y=y,
-        mode="markers",
-        marker=go.scatter.Marker(
-            size=sz, color=colors, opacity=0.6, colorscale="Viridis"
-        ),
-    )
-)
 
 # Convert the figure to png using kaleido
-image_data = fig.to_image(format="png", engine="kaleido")
+image_data = pie_df.to_image(format="png", engine="kaleido")
 
 # Create an io.BytesIO object which can be used by FPDF2
 image = io.BytesIO(image_data)
