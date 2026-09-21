bad416b0a875fadc857226bdc323c80664a2f0c5
ben rose <bros@2030districts.org>
2026-09-21 11:21:01 -0500

    adding month code

--- 1_Portfolio_Data.py
+++ 1_Portfolio_Data.py
@@ -24,14 +24,12 @@ require_login()
 
                                                         ###SUMMARY DATA - total sqfootage, buildings with complete data  
 st.title("Portfolio Data")
-
-if st.button("Refresh data"):
-    st.cache_data.clear()
-    st.rerun()
-
 conn = get_connection()
 tenant = get_current_tenant()
 most_recent_full_calendar_year = time.localtime().tm_year - 1
+if st.button("Refresh data"):
+    st.cache_data.clear()
+    st.rerun()
 
 def summary_query_builder(tenant):
     if tenant=='washtenaw':
